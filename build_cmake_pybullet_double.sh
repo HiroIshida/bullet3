8a1fbdebad67b9eb614e0f2ffe607f3ce0aa999d
HiroIshida <spitfire.docomo@gmail.com>
2020-07-01 17:19:35 +0900

    debug build

--- build_cmake_pybullet_double.sh
+++ build_cmake_pybullet_double.sh
@@ -3,9 +3,9 @@
 if [ -e CMakeCache.txt ]; then
   rm CMakeCache.txt
 fi
-mkdir -p build_cmake
-cd build_cmake
-cmake -DBUILD_PYBULLET=ON -DBUILD_PYBULLET_NUMPY=ON -DUSE_DOUBLE_PRECISION=ON -DBT_USE_EGL=ON -DCMAKE_BUILD_TYPE=Release .. || exit 1
+mkdir -p build
+cd build
+cmake -j 3 -DBUILD_PYBULLET=ON -DBUILD_PYBULLET_NUMPY=ON -DUSE_DOUBLE_PRECISION=ON -DBT_USE_EGL=ON -DCMAKE_BUILD_TYPE=Debug .. || exit 1
 make -j $(command nproc 2>/dev/null || echo 12) || exit 1
 cd examples
 cd pybullet
